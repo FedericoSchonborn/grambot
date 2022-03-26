cf8397ef2fbc11b01d1ef1304bfbec95625ced01
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-03-26 20:35:02 -0300

    Make GetChat a single item tuple
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- src/methods/get_chat.rs
+++ src/methods/get_chat.rs
@@ -3,17 +3,30 @@ use serde::Serialize;
 use crate::methods::types::ChatId;
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize)]
-pub struct GetChat {
-    pub chat_id: ChatId,
-}
+#[serde(into = "raw::GetChat")]
+pub struct GetChat(pub ChatId);
 
 impl GetChat {
     pub fn new<C>(chat_id: C) -> Self
     where
         C: Into<ChatId>,
     {
-        Self {
-            chat_id: chat_id.into(),
+        Self(chat_id.into())
+    }
+}
+
+mod raw {
+    #[allow(clippy::wildcard_imports)]
+    use super::*;
+
+    #[derive(Serialize)]
+    pub struct GetChat {
+        chat_id: ChatId,
+    }
+
+    impl From<super::GetChat> for GetChat {
+        fn from(it: super::GetChat) -> Self {
+            Self { chat_id: it.0 }
         }
     }
 }
