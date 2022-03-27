7ec7f9c82c082e264851246055b5cffd252b1e76
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-03-26 21:28:29 -0300

    Implement Display and FromStr for AllowedUpdate
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- src/methods/types.rs
+++ src/methods/types.rs
@@ -1,30 +1,11 @@
 //! Extra types used by method parameters.
 
-use serde::Serialize;
-
+mod allowed_update;
 mod chat_id;
 mod parse_mode;
 mod reply_markup;
 
+pub use allowed_update::*;
 pub use chat_id::*;
 pub use parse_mode::*;
 pub use reply_markup::*;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize)]
-#[serde(rename_all = "snake_case")]
-pub enum AllowedUpdate {
-    Message,
-    EditedMessage,
-    ChannelPost,
-    EditedChannelPost,
-    InlineQuery,
-    ChosenInlineResult,
-    CallbackQuery,
-    ShippingQuery,
-    PreCheckoutQuery,
-    Poll,
-    PollAnswer,
-    MyChatMember,
-    ChatMember,
-    CanJoinRequest,
-}
---
+++ src/methods/types/allowed_update.rs
@@ -0,0 +1,72 @@
+use std::{fmt::Display, str::FromStr};
+
+use serde::Serialize;
+use thiserror::Error;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum AllowedUpdate {
+    Message,
+    EditedMessage,
+    ChannelPost,
+    EditedChannelPost,
+    InlineQuery,
+    ChosenInlineResult,
+    CallbackQuery,
+    ShippingQuery,
+    PreCheckoutQuery,
+    Poll,
+    PollAnswer,
+    MyChatMember,
+    ChatMember,
+    CanJoinRequest,
+}
+
+impl Display for AllowedUpdate {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            Self::Message => "message",
+            Self::EditedMessage => "edited_message",
+            Self::ChannelPost => "channel_post",
+            Self::EditedChannelPost => "edited_channel_post",
+            Self::InlineQuery => "inline_query",
+            Self::ChosenInlineResult => "chosen_inline_result",
+            Self::CallbackQuery => "callback_query",
+            Self::ShippingQuery => "shipping_query",
+            Self::PreCheckoutQuery => "pre_checkout_query",
+            Self::Poll => "poll",
+            Self::PollAnswer => "poll_answer",
+            Self::MyChatMember => "my_chat_member",
+            Self::ChatMember => "chat_member",
+            Self::CanJoinRequest => "can_join_request",
+        })
+    }
+}
+
+#[derive(Debug, Error)]
+#[error("expected one of `message`, `edited_message`, `channel_post`, `edited_channel_post`, `inline_query`, `chosen_inline_result`, `callback_query`, `shipping_query`, `pre_checkout_query`, `poll`, `poll_answer`, `my_chat_member`, `chat_member`, `can_join_request`")]
+pub struct TryFromAllowedUpdateError;
+
+impl FromStr for AllowedUpdate {
+    type Err = TryFromAllowedUpdateError;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        match value {
+            "message" => Ok(Self::Message),
+            "edited_message" => Ok(Self::EditedMessage),
+            "channel_post" => Ok(Self::ChannelPost),
+            "edited_channel_post" => Ok(Self::EditedChannelPost),
+            "inline_query" => Ok(Self::InlineQuery),
+            "chosen_inline_result" => Ok(Self::ChosenInlineResult),
+            "callback_query" => Ok(Self::CallbackQuery),
+            "shipping_query" => Ok(Self::ShippingQuery),
+            "pre_checkout_query" => Ok(Self::PreCheckoutQuery),
+            "poll" => Ok(Self::Poll),
+            "poll_answer" => Ok(Self::PollAnswer),
+            "my_chat_member" => Ok(Self::MyChatMember),
+            "chat_member" => Ok(Self::ChatMember),
+            "can_join_request" => Ok(Self::CanJoinRequest),
+            _ => Err(TryFromAllowedUpdateError),
+        }
+    }
+}
