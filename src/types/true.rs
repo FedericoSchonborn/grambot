a89c347613435c476f84e571c4e5446132eacfea
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-04-29 01:14:48 -0300

    Simplify True's Deserialize implementation
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- src/types/true.rs
+++ src/types/true.rs
@@ -1,7 +1,5 @@
-use std::fmt::{self, Formatter};
-
 use serde::{
-    de::{Error as DeError, Unexpected, Visitor},
+    de::{Error as DeError, Unexpected},
     Deserialize, Deserializer,
 };
 
@@ -13,27 +11,14 @@ impl<'de> Deserialize<'de> for True {
     where
         D: Deserializer<'de>,
     {
-        struct TrueVisitor;
-
-        impl<'de> Visitor<'de> for TrueVisitor {
-            type Value = True;
-
-            fn expecting(&self, f: &mut Formatter) -> fmt::Result {
-                f.write_str("the literal `true` value")
-            }
-
-            fn visit_bool<E>(self, value: bool) -> Result<Self::Value, E>
-            where
-                E: DeError,
-            {
-                if value {
-                    Ok(True)
-                } else {
-                    Err(DeError::invalid_value(Unexpected::Bool(false), &self))
-                }
-            }
+        let value = bool::deserialize(deserializer)?;
+        if value {
+            Ok(Self)
+        } else {
+            Err(DeError::invalid_value(
+                Unexpected::Bool(value),
+                &"a boolean literal `true`",
+            ))
         }
-
-        deserializer.deserialize_bool(TrueVisitor)
     }
 }
